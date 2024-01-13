74474f81dfcf679d03dce44d69503e865c521137
Luis Reyes <luisfreyees@gmail.com>
2024-01-13 11:18:58 -0600

    Locators comments errased

--- ConfigurationProvider/Logger.cs
+++ ConfigurationProvider/Logger.cs
@@ -5,17 +5,11 @@ namespace ConfigurationProvider
     public static class Logger
     {
         private static readonly string _lock = "";
-        //public static ISpecFlowOutputHelper specflowLogger;
-        public static TechTalk.SpecFlow.Infrastructure.SpecFlowOutputHelper specflowLogger;
 
         public static void WriteLine(string message, bool useSpecflowLogger = false)
         {
             lock (_lock)
             {
-                if (useSpecflowLogger && specflowLogger != null)
-                {
-                    specflowLogger.WriteLine(message);
-                }
                 NUnit.Framework.TestContext.Progress.WriteLine(DateTime.Now.ToString("yyyyMMdd HH:mm:ss.fff") + " - " + message);
             }
         }
--- TestSolutionSpecflow/PageObjects/MainPage.locator.cs
+++ TestSolutionSpecflow/PageObjects/MainPage.locator.cs
@@ -36,29 +36,10 @@ namespace TestSolution.PageObjects
         [Locator(LocatorType.XPath, "//input[@id='NewItemAddButton']")]
         public Button? AddNewItemButton { get; }
 
+        [Element("ItemBox", ElementType.TextBox)]
+        [Locator(LocatorType.XPath, "//div[@class='ItemContentDiv' and text()='{0}']")]
+        public TextBox? ItemBox { get; }
 
-
-        //private readonly By _logOutButton = By.XPath("//a[@id='ctl00_HeaderTopControl1_LinkButtonLogout']");
-        //private readonly By _addNewProjectButton = By.XPath("//td[text()='Add New Project']");
-        //private readonly By _newProjectTextbox = By.XPath("//td[@class='ProjItemContent']//input[@id='NewProjNameInput']");
-        //private readonly By _addNewProjectFinalButton = By.XPath("//td[@class='ProjItemContent']//input[@id='NewProjNameButton']");
-        //private readonly By _newProjectBox = By.XPath("//td[@class='ProjItemContent' and text()='Errands']");
-        //private readonly By _newItemBox = By.XPath("//textarea[@class='InputTextAddItem InputTextAddItemWatermark']");
-        //private readonly By _addNewItemFinalButton = By.XPath("//input[@id='NewItemAddButton']");
-        private readonly By _newItem = By.XPath("//div[@class='ItemContentDiv' and text()='Pasta']");
-
-
-
-
-
-       // private IWebElement LogOutButton => _driver.FindElement(_logOutButton);
-       // private IWebElement AddNewProjectButton => _driver.FindElement(_addNewProjectButton);
-       // private IWebElement NewProjectTextbox => _driver.FindElement(_newProjectTextbox);
-       // private IWebElement AddNewProjectFinalButton => _driver.FindElement(_addNewProjectFinalButton);
-       // private IWebElement NewProjectBox => _driver.FindElement(_newProjectBox);
-       // private IWebElement NewItemBox => _driver.FindElement(_newItemBox);
-       // private IWebElement AddNewItemButton => _driver.FindElement(_addNewItemFinalButton);
-        private IWebElement NewItem => _driver.FindElement(_newItem);
 
     }
 }
