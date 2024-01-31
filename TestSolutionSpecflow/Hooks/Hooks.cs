9bf0f5615cd3a890b7a509d2985564f4db02f911
Luis Reyes <luisfreyees@gmail.com>
2024-01-31 09:50:00 -0600

    All the comments in hooks file have been deleted

--- TestSolutionSpecflow/Hooks/Hooks.cs
+++ TestSolutionSpecflow/Hooks/Hooks.cs
@@ -164,19 +164,6 @@ namespace TestSolution.Hooks
             var client = new RestHelper(RestConfig.Url, RestConfig.Email, RestConfig.Password);
             var responseGetItems = client.DoRequest(Method.Delete, "Filters/-4/Items.json", null);
 
-            /*var responseGetItems = client.DoRequest(Method.Get, "Projects/-3/ItemsWithInfo.json", null);
-            var items = JsonSerializer.Deserialize<IEnumerable<ProjectModel>>(responseGetItems.Content!);
-
-            foreach (var item in items!)
-            {
-                string url = $"Projects/-3/ItemsWithInfo/{item.Id}.json";
-                var responseDelItems = client.DoRequest(Method.Delete, url, null);
-                if (!responseDelItems.IsSuccessful)
-                {
-                    throw new Exception($"Unable to Delete the Items.");
-                }
-            }
-            */
         }
     }
 }
